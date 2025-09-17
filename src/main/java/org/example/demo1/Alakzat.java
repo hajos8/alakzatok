6e3f29bfcd02fee3112ec55807aef000b585c4d2
hajos8 <rabb.andras@gmail.com>
2025-09-17 13:16:16 +0200

    File readng, file writing

--- src/main/java/org/example/demo1/Alakzat.java
+++ src/main/java/org/example/demo1/Alakzat.java
@@ -1,4 +1,11 @@
 package org.example.demo1;
 
 public class Alakzat {
+    public String color;
+    public String shape;
+
+    public Alakzat(String color, String shape) {
+        this.color = color;
+        this.shape = shape;
+    }
 }
--- src/main/java/org/example/demo1/AlakzatController.java
+++ src/main/java/org/example/demo1/AlakzatController.java
@@ -1,8 +1,127 @@
 package org.example.demo1;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
-public class AlakzatController {
+import java.io.File;
+import java.io.FileWriter;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
+public class AlakzatController implements Initializable {
+    @FXML public RadioButton red, blue, green;
+    @FXML public RadioButton circle, square, triangle;
+    @FXML public ListView<String> list;
+    @FXML public ImageView imageView;
+    @FXML public VBox imgBackgrond;
+
+    public static ArrayList<Alakzat> alakzatokList = new ArrayList<>();
+
+    @FXML
+    protected void fillUp(){
+        ObservableList <String> lines = FXCollections.observableArrayList();
+        for (Alakzat a : alakzatokList) {
+            lines.add(a.color + ", " + a.shape);
+        }
+        list.setItems(lines);
+    }
+
+    @FXML
+    protected void add(){
+        String color = red.isSelected() ? "Piros" : blue.isSelected() ? "Kék" : green.isSelected() ? "Zöld" : "";
+        String shape = circle.isSelected() ? "Kör" : square.isSelected() ? "Négyzet" : triangle.isSelected() ? "Háromszög" : "";
+
+        alakzatokList.add(new Alakzat(color, shape));
+
+        fillUp();
+    }
+
+    @FXML
+    protected void delete(){
+        alakzatokList.remove(list.getSelectionModel().getSelectedIndex());
+
+        fillUp();
+    }
+
+    @FXML
+    protected void changeBg(){
+        if(red.isSelected()){
+            imgBackgrond.setStyle("-fx-background-color: red;-fx-border-color: black; -fx-border-width: 3px;");
+        }
+        else if(blue.isSelected()){
+            imgBackgrond.setStyle("-fx-background-color: blue;-fx-border-color: black; -fx-border-width: 3px;");
+        }
+        else if(green.isSelected()){
+            imgBackgrond.setStyle("-fx-background-color: green;-fx-border-color: black; -fx-border-width: 3px;");
+        }
+    }
+
+    @FXML
+    protected void changeImg(){
+        if(circle.isSelected()){
+            createImg("/icons/kor.png");
+        }
+        else if(square.isSelected()){
+            createImg("/icons/negyzet.png");
+        }
+        else if(triangle.isSelected()){
+            createImg("/icons/haromszog.png");
+        }
+    }
+
+    @FXML
+    protected void createImg(String path){
+        if(path == null || path.isEmpty())
+            return;
+
+        Image image = new Image(getClass().getResourceAsStream(path));
+        imageView.setImage(image);
+    }
+
+    @FXML
+    protected void save(){
+        try{
+            FileWriter writer = new FileWriter("alakzat.dat", StandardCharsets.UTF_8, false);
+            for(Alakzat a : alakzatokList){
+                writer.write(a.color + ", " + a.shape + "\n");
+                writer.close();
+            }
+        }
+        catch (Exception e){
+            System.out.println("Hiba a fájl írásakor: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            Scanner scanner = new Scanner(new File("alakzat.dat"), StandardCharsets.UTF_8);
+
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                String[] parts = line.split(", ");
+                alakzatokList.add(new Alakzat(parts[0], parts[1]));
+
+            }
+
+            fillUp();
+
+            scanner.close();
+
+        }
+        catch (Exception e){
+            System.out.println("Hiba a fájl megnyitásakor: " + e.getMessage());
+        }
+
+    }
 }
